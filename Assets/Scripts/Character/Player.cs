cf6f46e80314f95c73fc853b2e4baa1412ea302c
phamkhoavp <52957433+phamkhoavp@users.noreply.github.com>
2020-12-03 22:58:42 +0700

    done

---
+++ Assets/Scripts/Character/Player.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour {
+
+    [SerializeField]
+    private AudioSource audioSource;
+    [SerializeField]
+    private AudioClip playerMove, enemyDeath;
+
+    public float speed;
+    public float increment;
+    public float maxY;
+    public float minY;
+
+    private Vector2 targetPos;
+
+    public int health;
+
+    public GameObject moveEffect;
+    public Animator camAnim;
+    public Text healthDisplay;
+
+    public GameObject spawner;
+    public GameObject restartDisplay;
+
+    private void Update()
+    {
+        if (health <= 0) {
+            spawner.SetActive(false);
+            restartDisplay.SetActive(true);
+            Destroy(gameObject);
+        }
+        healthDisplay.text = health.ToString();
+
+        transform.position = Vector2.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.UpArrow) && transform.position.y < maxY) {
+            audioSource.PlayOneShot(playerMove);
+            camAnim.SetTrigger("shake");
+            Instantiate(moveEffect, transform.position, Quaternion.identity);
+            targetPos = new Vector2(transform.position.x, transform.position.y + increment);
+        } else if (Input.GetKeyDown(KeyCode.DownArrow) && transform.position.y > minY) {
+            audioSource.PlayOneShot(playerMove);
+            camAnim.SetTrigger("shake");
+            Instantiate(moveEffect, transform.position, Quaternion.identity);
+            targetPos = new Vector2(transform.position.x, transform.position.y - increment);
+        }
+    }
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Enemy"))
+        {
+            audioSource.PlayOneShot(enemyDeath);
+        }
+    }
+}
---
+++ Assets/Scripts/GameController/MainmenuController.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MainmenuController : MonoBehaviour
+{
+    public void OnPlayButtonClick()
+    {
+        string currentLevel = PlayerPrefs.GetString("CURRENT_LEVEL", "Game");
+        SceneManager.LoadScene(currentLevel);
+    }
+}
